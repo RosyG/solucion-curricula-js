f8a871dc3488b722d0280ea02801c0658f2f2318
RosyG <rmga2106@gmail.com>
2017-10-17 09:36:20 -0500

    Subiendo soluciones de PreAdmi

---
+++ mi primera web/app.js
@@ -0,0 +1,6 @@
+      alert ("¡Confirma tu verdadera edad!")
+      prompt ("¿Qué edad tienes, mmm?")
+document.write("Hola" + "mundo");
+var age=123;
+document.write("Me \"encanta\" JavaScript" );
+ console.log( "alphabet".toUpperCase() )
---
+++ uso de funtion/app.js
@@ -0,0 +1,6 @@
+var dar=funtion(valor1){
+for var i=1; i<=valor1;i++){
+console.log ('X')
+}
+}
+dar(8);
